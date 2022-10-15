bf1e113410a7b285e122e8240d186bb4a99460f7
Michael <michaelgandolfi7@gmail.com>
2022-10-15 17:00:41 -0500

    implemented win conditional

---
+++ js/main.js
@@ -0,0 +1,160 @@
+let waitingOn0 = false;
+let cell0 = ""
+let cell1 = ""
+let cell2 = ""
+let cell3 = ""
+let cell4 = ""
+let cell5 = ""
+let cell6 = ""
+let cell7 = ""
+let cell8 = ""
+
+document.querySelector("#cell0").addEventListener("click", cell0Move);
+document.querySelector("#cell1").addEventListener("click", cell1Move);
+document.querySelector("#cell2").addEventListener("click", cell2Move);
+document.querySelector("#cell3").addEventListener("click", cell3Move);
+document.querySelector("#cell4").addEventListener("click", cell4Move);
+document.querySelector("#cell5").addEventListener("click", cell5Move);
+document.querySelector("#cell6").addEventListener("click", cell6Move);
+document.querySelector("#cell7").addEventListener("click", cell7Move);
+document.querySelector("#cell8").addEventListener("click", cell8Move);
+
+
+
+function cell0Move() {
+    if (waitingOn0 === false) {
+        cell0 = document.querySelector("#cell0").innerText = "X"
+        waitingOn0 = true;
+        checkXWin()
+        checkOWin()
+    } else {
+        cell0 = document.querySelector("#cell0").innerText = "O"
+        waitingOn0 = false;
+        checkXWin()
+        checkOWin()
+    }
+}
+
+function cell1Move() {
+    if (waitingOn0 === false) {
+        cell1 = document.querySelector("#cell1").innerText = "X"
+        waitingOn0 = true;
+        checkXWin()
+        checkOWin()
+    } else {
+        cell1 = document.querySelector("#cell1").innerText = "O"
+        waitingOn0 = false;
+        checkXWin()
+        checkOWin()
+    }
+}
+
+function cell2Move() {
+    if (waitingOn0 === false) {
+        cell2 = document.querySelector("#cell2").innerText = "X"
+        waitingOn0 = true;
+        checkXWin()
+        checkOWin()
+    } else {
+        cell2 = document.querySelector("#cell2").innerText = "O"
+        waitingOn0 = false;
+        checkXWin()
+        checkOWin()
+    }
+}
+
+function cell3Move() {
+    if (waitingOn0 === false) {
+        cell3 = document.querySelector("#cell3").innerText = "X"
+        waitingOn0 = true;
+        checkXWin()
+        checkOWin()
+    } else {
+        cell3 = document.querySelector("#cell3").innerText = "O"
+        waitingOn0 = false;
+        checkXWin()
+        checkOWin()
+    }
+}
+
+function cell4Move() {
+    if (waitingOn0 === false) {
+        cell4 = document.querySelector("#cell4").innerText = "X"
+        waitingOn0 = true;
+        checkXWin()
+        checkOWin()
+    } else {
+        cell4 = document.querySelector("#cell4").innerText = "O"
+        waitingOn0 = false;
+        checkXWin()
+        checkOWin()
+    }
+}
+
+function cell5Move() {
+    if (waitingOn0 === false) {
+        cell5 = document.querySelector("#cell5").innerText = "X"
+        waitingOn0 = true;
+        checkXWin()
+        checkOWin()
+    } else {
+        cell5 = document.querySelector("#cell5").innerText = "O"
+        waitingOn0 = false;
+        checkXWin()
+        checkOWin()
+    }
+}
+
+function cell6Move() {
+    if (waitingOn0 === false) {
+        cell6 = document.querySelector("#cell6").innerText = "X"
+        waitingOn0 = true;
+        checkXWin()
+        checkOWin()
+    } else {
+        cell6 = document.querySelector("#cell6").innerText = "O"
+        waitingOn0 = false;
+        checkXWin()
+        checkOWin()
+    }
+}
+
+function cell7Move() {
+    if (waitingOn0 === false) {
+        cell7 = document.querySelector("#cell7").innerText = "X"
+        waitingOn0 = true;
+        checkXWin()
+        checkOWin()
+    } else {
+        cell7 = document.querySelector("#cell7").innerText = "O"
+        waitingOn0 = false;
+        checkXWin()
+        checkOWin()
+    }
+}
+
+function cell8Move() {
+    if (waitingOn0 === false) {
+        cell8 = document.querySelector("#cell8").innerText = "X"
+        waitingOn0 = true;
+        checkXWin()
+        checkOWin()
+    } else {
+        cell8 = document.querySelector("#cell8").innerText = "O"
+        waitingOn0 = false;
+        checkXWin()
+        checkOWin()
+    }
+}
+
+function checkXWin() {
+    if (cell0 === "X" && cell1 === "X" && cell2 === "X" || cell3 === "X" && cell4 === "X" && cell5 === "X" || cell6 === "X" && cell7 === "X" && cell8 === "X" || cell0 === "X" && cell3 === "X" && cell6 === "X" || cell1 === "X" && cell4 === "X" && cell7 === "X" || cell2 === "X" && cell5 === "X" && cell8 === "X" || cell0 === "X" && cell4 === "X" && cell8 === "X") {
+        alert("X WINS!")
+} 
+}
+
+function checkOWin() {
+    if (cell0 === "O" && cell1 === "O" && cell2 === "O" || cell3 === "O" && cell4 === "O" && cell5 === "O" || cell6 === "O" && cell7 === "O" && cell8 === "O" || cell0 === "O" && cell3 === "O" && cell6 === "O" || cell1 === "O" && cell4 === "O" && cell7 === "O" || cell2 === "O" && cell5 === "O" && cell8 === "O" || cell0 === "O" && cell4 === "O" && cell8 === "O") {
+        alert("O WINS!")
+} 
+}
